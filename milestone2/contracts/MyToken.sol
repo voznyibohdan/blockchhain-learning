e51b48fc14f5c7565e7db92070f4d37e0009579e
voznyibohdan <bohdanvoznyi.em@gmail.com>
2024-01-03 12:31:28 +0200

    added IERC20 interface

--- milestone2/contracts/MyToken.sol
+++ milestone2/contracts/MyToken.sol
@@ -1,8 +1,20 @@
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
 contract MyToken is IERC20, Ownable, SafeMath {
     using SafeMath for uint256;
