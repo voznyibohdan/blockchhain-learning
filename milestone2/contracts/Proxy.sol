74fe6d911f22801248f9bdf29663ae0b30317ee4
voznyibohdan <bohdanvoznyi.em@gmail.com>
2024-01-05 08:02:43 +0200

    created proxy contract

---
+++ milestone2/contracts/Proxy.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Proxy {
+    address public implementation;
+    address public owner;
+
+    constructor(address _implementation) {
+        _initialize(msg.sender, _implementation);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function _initialize(address _owner, address _implementation) private {
+        require((_owner != address(0) && (_implementation != address(0))), "Zero address");
+        owner = _owner;
+        implementation = _implementation;
+    }
+
+    function updateImplementation(address _implementation) external onlyOwner {
+        require(_implementation != address(0), "Zero address");
+        implementation = _implementation;
+    }
+
+    fallback() external payable {
+        address _impl = implementation;
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), _impl, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            switch result
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
+        }
+    }
+}
